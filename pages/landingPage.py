c4cefce126dbb7e6815ed49016dc80fa51124c82
Mansoor Aleep <mansoor.aleep@SAO-MC-190509C.local>
2020-03-23 21:26:54 -0600

    fix on landing page

--- pages/landingPage.py
+++ pages/landingPage.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 class LandingPage():
 
     def __init__(self, driver):
@@ -7,5 +11,7 @@ class LandingPage():
 
     def go_to_shopping_cart(self):
         self.driver.find_element_by_xpath(self.addToCart_button_Xpath).click()
-        self.driver.implicitly_wait(10)
-        self.driver.find_element_by_link_text(self.proceedToCheckout_LinkText).click()
+        #self.driver.implicitly_wait(10)
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, self.proceedToCheckout_LinkText)))
+        element.click()
+       # self.driver.find_element_by_link_text(self.proceedToCheckout_LinkText).click()
